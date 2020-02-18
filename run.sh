40ed533e7d086209c71bb690bdeb574195831f4a
mkirsche <jnredotter@gmail.com>
2020-02-18 10:12:26 -0500

    Lowered max distance parameter to avoid false merges

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
  javac -cp /work-zfs/mschatz1/pipelines/soft/git/Jasmine/src src/*.java
-java -cp /work-zfs/mschatz1/pipelines/soft/git/Jasmine/src:src Rescreen merged_vcf=/scratch/groups/mschatz1/mkirsche/jasminepost/merged_clean_500_precise.vcf bam_file_list=/scratch/groups/mschatz1/mkirsche/jasminepost/bamfilelist.txt out_file=test.vcf sniffles_path=/work-zfs/mschatz1/pipelines/soft/sniffles samtools_path=/work-zfs/mschatz1/pipelines/soft/samtools sniffles_max_dist=50 | tee log.txt
+java -cp /work-zfs/mschatz1/pipelines/soft/git/Jasmine/src:src Rescreen merged_vcf=/scratch/groups/mschatz1/mkirsche/jasminepost/merged_clean_500_precise.vcf bam_file_list=/scratch/groups/mschatz1/mkirsche/jasminepost/bamfilelist.txt out_file=test.vcf sniffles_path=/work-zfs/mschatz1/pipelines/soft/sniffles samtools_path=/work-zfs/mschatz1/pipelines/soft/samtools sniffles_max_dist=50 --run_all --update_id_lists max_dist=50 | tee log.txt
 
